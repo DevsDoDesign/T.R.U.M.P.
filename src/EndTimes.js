f20e44b9f9da18c330e382c13811563180ba0ad7
Alasdair Smith <ali@alasdairsmith.co.uk>
2016-09-16 18:39:18 +0100

    Nuke animation

--- src/EndTimes.js
+++ src/EndTimes.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import button from '../assets/big-red-button.png'
+import hand from '../assets/hand.jpg'
 import './EndTimes.css'
 
 export default class EndTimes extends Component {
@@ -14,6 +16,17 @@ export default class EndTimes extends Component {
 				<h1>Congratulations!</h1>
 				<p>You've demoralised T.R.U.M.P and his minions</p>
 				<button onClick={this.nextStage} className="EndTimes__button">Next</button>
+			</div>
+		)
+	}
+
+	renderBigRedButton = () => {
+		setTimeout(this.nextStage, 1500)
+
+		return (
+			<div className="EndTimes EndTimes--nuke">
+				<img src={hand} height="200" className="EndTimes__hand-animation" />
+				<img src={button} height="350" className="EndTimes__bigred" />
 			</div>
 		)
 	}
@@ -39,8 +52,9 @@ export default class EndTimes extends Component {
 	render() {
 		switch (this.state.stage) {
 			case 1: return this.renderCongrats()
-			case 2: return this.renderGif()
-			case 3: return this.renderGameOver()
+			case 2: return this.renderBigRedButton()
+			case 3: return this.renderGif()
+			case 4: return this.renderGameOver()
 		}
 	}
 }
