2ca91792db4a81e01f9474f6439cd02ca19aaad8
Dan Harper <intouch@danharper.me>
2016-09-16 17:56:51 +0100

    snaps

--- src/Snapchat.js
+++ src/Snapchat.js
@@ -5,24 +5,38 @@ import snap1 from './snaps/1.png'
 import snapBalls from './snaps/balls.jpg'
 import snapMario from './snaps/mario.jpg'
 import snapPubHack from './snaps/pubhack.png'
+import snapO from './snaps/o.jpg'
+import snapP from './snaps/P.jpg'
+import snapl from './snaps/l.png'
 import twitter from './twitter-logo-from-guidelines.png'
 import bg from './sky.jpg'
 
 const SNAPS = [
-	{left: 0, delay: '0s', src: snap1},
-	{left: 200, delay: '.5s', src: snapBalls},
-	{left: 140, delay: '1s', src: snap1},
-	{left: 40, delay: '2s', src: snapMario},
+	{left: 0, delay: '0s', src: snapl},
+	{left: 200, delay: '.5s', src: snapO},
+	{left: 140, delay: '1s', src: snapP},
+	{left: 40, delay: '2s', src: snapP},
 	{left: 60, delay: '2.5s', src: snapBalls},
-	{left: 200, delay: '3s', src: snapPubHack},
+	{left: 200, delay: '3s', src: snapP},
 	{left: 0, delay: '3.5s', src: snapMario},
-	{left: 100, delay: '5s', src: snapPubHack},
-	{left: 60, delay: '5.3s', src: snapBalls},
+	{left: 100, delay: '5s', src: snapBalls},
+	{left: 60, delay: '5.3s', src: snapP},
 	{left: 290, delay: '5.4s', src: snapPubHack},
-	{left: 80, delay: '5.5s', src: snapMario},
+	{left: 80, delay: '5.5s', src: snapO},
 	{left: 0, delay: '6s', src: snapBalls},
-	{left: 200, delay: '6.5s', src: snapBalls},
-	{left: 140, delay: '6.6s', src: snapPubHack},
+	{left: 200, delay: '6.5s', src: snapl},
+	{left: 140, delay: '7s', src: snapBalls},
+
+	{left: 0, delay: '7.5s', src: snapO},
+	{left: 50, delay: '8.1s', src: snapP},
+	{left: 100, delay: '9.2s', src: snapPubHack},
+	{left: 200, delay: '10s', src: snapPubHack},
+	{left: 150, delay: '11.9s', src: snapO},
+	{left: 200, delay: '12.6s', src: snapP},
+	{left: 250, delay: '13.0s', src: snapBalls},
+	{left: 0, delay: '14.8s', src: snapl},
+	{left: 50, delay: '15.9s', src: snapPubHack},
+	{left: 160, delay: '18s', src: snapl},
 ]
 
 class Snapchat extends Component {
