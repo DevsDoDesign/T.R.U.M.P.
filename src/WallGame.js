3afad72fbec668effe8edfb46a17292cba2832b0
Alasdair Smith <ali@alasdairsmith.co.uk>
2016-09-16 17:34:20 +0100

    Fix wall with final assets

--- src/WallGame.js
+++ src/WallGame.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 import introOutro from './introOutro'
 import image1 from '../assets/wall/1.jpg'
 import image2 from '../assets/wall/2.jpg'
+import image3 from '../assets/wall/3.jpg'
+import image4 from '../assets/wall/4.jpg'
+import image5 from '../assets/wall/5.jpg'
 import rock from '../assets/rock.png'
 import theRock from '../assets/the-rock.png'
 import './WallGame.css'
 
-const NO_OF_THROWS = 2
+const NO_OF_THROWS = 5
 
 class WallGame extends Component {
 	state = {
@@ -44,6 +47,21 @@ class WallGame extends Component {
 			opacity: this.state.image === 2 ? 1 : 0
 		}
 
+		const wall3Style = {
+			backgroundImage: `url(${image3})`,
+			opacity: this.state.image === 3 ? 1 : 0
+		}
+
+		const wall4Style = {
+			backgroundImage: `url(${image4})`,
+			opacity: this.state.image === 4 ? 1 : 0
+		}
+
+		const wall5Style = {
+			backgroundImage: `url(${image5})`,
+			opacity: this.state.image === 5 ? 1 : 0
+		}
+
 		let rockClass = 'Wall__Rock '
 		if (this.state.isAnimating) rockClass += 'Wall__Rock--animated'
 
@@ -51,6 +69,9 @@ class WallGame extends Component {
 			<div onClick={this.throwRock}>
 				<div className="Wall__Wall" style={wall1Style}></div>
 				<div className="Wall__Wall" style={wall2Style}></div>
+				<div className="Wall__Wall" style={wall3Style}></div>
+				<div className="Wall__Wall" style={wall4Style}></div>
+				<div className="Wall__Wall" style={wall5Style}></div>
 				<img src={this.state.canYouSmellWhatTheRockIsCooking ? theRock : rock} className={rockClass}/>
 			</div>
 		)
