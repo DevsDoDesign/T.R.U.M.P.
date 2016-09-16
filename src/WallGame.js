1adcb87a76832a5e057169a8b613d819ba9ef604
Alasdair Smith <ali@alasdairsmith.co.uk>
2016-09-16 14:58:01 +0100

    Shows outro after all wall is destroyed

--- src/WallGame.js
+++ src/WallGame.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
+import InfoCard from './InfoCard'
 import image1 from '../assets/wall/1.jpg'
 import image2 from '../assets/wall/2.jpg'
 import rock from '../assets/the-rock.png'
 import './WallGame.css'
 
+const NO_OF_THROWS = 6
+
 class WallGame extends Component {
 	state = {
 		image: 1,
-		isAnimating: false
+		isAnimating: false,
+		showOutro: false
 	}
 
 	showNext = () => {
+		if (this.state.image === NO_OF_THROWS) {
+			this.setState({ showOutro: true })
+		}
+		else {
+			this.throwRock()
+		}
+	}
+
+	throwRock () {
 		this.setState({ isAnimating: true })
 
 		setTimeout(() => {
@@ -22,7 +35,7 @@ class WallGame extends Component {
 		}, 800)
 	}
 
-	render() {
+	renderGame () {
 		const wall1Style = {
 			backgroundImage: `url(${image1})`,
 			opacity: this.state.image === 1 ? 1 : 0
@@ -40,9 +53,28 @@ class WallGame extends Component {
 			<div onClick={this.showNext}>
 				<div className="Wall__Wall" style={wall1Style}></div>
 				<div className="Wall__Wall" style={wall2Style}></div>
-				<img src={rock} className={rockClass} />
+				<img src={rock} className={rockClass}/>
 			</div>
 		)
+	}
+
+	renderOutro() {
+		return (
+			<InfoCard
+				title="You've brought down the wall!"
+				content="Well done! You've let thousands of Mexicans into the US!"
+				onOk={this.props.onFinish}
+			/>
+		)
+	}
+
+	render() {
+		if (this.state.showOutro) {
+			return this.renderOutro()
+		}
+		else {
+			return this.renderGame()
+		}
 	}
 }
 
