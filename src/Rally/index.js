ef94bc373d833b5618e6a9311880fd76586daf33
Robb Lewis <me@therobb.com>
2016-09-16 15:15:57 +0100

    TRUMP

--- src/Rally/index.js
+++ src/Rally/index.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react'
-import attendee from './attendee.png'
+import attendees from './people-resized.png'
+import rally from './trump-rally.png'
+import sad from './sad.png'
 import css from './styles.css'
 
 class Rally extends Component {
 
 	state = {
-		stage: 0
+		stage: 0,
+		sad: false,
 	}
 
 	render() {
 		return (
-			<div className="rally-bg" onClick={this.shout}>
-				<img src={attendee} className={this.state.stage >= 1 ? 'rally__attendees-1 rally__active__right': 'rally__attendees-1'}/>
-				<img src={attendee} className={this.state.stage >= 3 ? 'rally__attendees-2 rally__active__left': 'rally__attendees-2'}/>
-				<img src={attendee} className={this.state.stage >= 3 ? 'rally__attendees-3 rally__active__right': 'rally__attendees-3'}/>
-				<img src={attendee} className={this.state.stage >= 2 ? 'rally__attendees-4 rally__active__left': 'rally__attendees-4'}/>
-				<img src={attendee} className={this.state.stage >= 2 ? 'rally__attendees-5 rally__active__right': 'rally__attendees-5'}/>
-				<img src={attendee} className={this.state.stage >= 1 ? 'rally__attendees-6 rally__active__left': 'rally__attendees-6'}/>
+			<div className="rally-bg" onClick={this.shout} style={{backgroundImage: 'url(' + rally + ')'}}>
+				{this.state.sad &&  <img src={sad} className="rally__sad"/>}
+				<img src={attendees} className={this.state.stage >= 1 ? 'rally__attendees-1 rally__active__right': 'rally__attendees-1'}/>
+				<img src={attendees} className={this.state.stage >= 3 ? 'rally__attendees-2 rally__active__left': 'rally__attendees-2'}/>
+				<img src={attendees} className={this.state.stage >= 2 ? 'rally__attendees-3 rally__active__right': 'rally__attendees-3'}/>
+				<img src={attendees} className={this.state.stage >= 1 ? 'rally__attendees-4 rally__active__left': 'rally__attendees-4'}/>
+				<img src={attendees} className={this.state.stage >= 3 ? 'rally__attendees-5 rally__active__right': 'rally__attendees-5'}/>
+				<img src={attendees} className={this.state.stage >= 2 ? 'rally__attendees-6 rally__active__left': 'rally__attendees-6'}/>
 			</div>
 		)
 	}
@@ -47,7 +51,10 @@ class Rally extends Component {
 	stageThree = () => {
 		(new Audio('http://webhooker.dev/boo.m4a')).play();
 		setTimeout(() => {
-			this.setState({ stage: 3 });
+			this.setState({
+				stage: 3,
+				sad: true
+			});
 		}, 6500)
 	}
 
